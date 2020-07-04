89c363f8cb7ffcf05b059e272c53fe8e7396d886
Adriel Kim <theadrielkim@gmail.com>
2020-07-03 22:42:55 -0400

    initial commit

---
+++ processView.py
@@ -0,0 +1,19 @@
+import lsst.daf.persistence as dafPersist
+import lsst.afw.display as afwDisplay
+butler = dafPersist.Butler(inputs='/home/adriel/DATA/rerun/processCcdOutputs')
+data = butler.queryMetadata('calexp', ['visit', 'ccd'], dataId={'filter': 'HSC-R'})
+print(data)
+calexp = butler.get('calexp',dataId = {'filter':'HSC-R', 'visit':903334, 'ccd':23})
+display = afwDisplay.getDisplay(backend = 'ds9')
+display.mtv(calexp)
+
+"""
+src = butler.get('src', dataId = {'filter':'HSC-R', 'visit':903334, 'ccd':23})
+print(len(src))
+#print(src.getSchema())
+print(src.schema.find("calib_psf_used"))
+
+mask = calexp.getMask()
+for maskName, maskBit in mask.getMaskPlaneDict().items():
+    print('{}: {}'.format(maskName, display.getMaskPlaneColor(maskName)))
+"""
